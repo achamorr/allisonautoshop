56cd2fe0df71174f41e9753f42f4d28f6ec45bd3
Daniel Chamorro <danielchamorro.q@gmail.com>
2020-05-26 15:22:16 -0500

    basis of navbar done, needs beautifying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-
-
-
 $(document).ready(function( $ ){
 
 
     //Navbar
         // When the user scrolls the page, execute myFunction
-        window.onscroll = function() {myFunction()};
+        window.onscroll = function() 
+        {
+            myFunction()
+        };
 
         // Get the navbar
         var navbar = document.getElementById("navbar");
@@ -16,11 +16,13 @@ $(document).ready(function( $ ){
 
         // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
         function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-        } else {
-            navbar.classList.remove("sticky");
-        }
+            if (window.pageYOffset >= sticky) 
+            {
+                navbar.classList.add("sticky")
+            } else 
+            {
+                navbar.classList.remove("sticky");
+            }
         }
 
 
@@ -112,14 +114,6 @@ $(document).ready(function( $ ){
         });
 
         //Pasted content ends
-
-
-        
-
-
-
-
-
 
 }); // end of $(document).ready(function()
 
