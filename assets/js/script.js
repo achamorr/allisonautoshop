b0a5853f506184ee504026877d59355fc334cf04
Daniel Chamorro <danielchamorro.q@gmail.com>
2020-05-25 22:51:25 -0500

    almost there with navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
+
+
+
 $(document).ready(function( $ ){
+
+
+    //Navbar
+        // When the user scrolls the page, execute myFunction
+        window.onscroll = function() {myFunction()};
+
+        // Get the navbar
+        var navbar = document.getElementById("navbar");
+
+        // Get the offset position of the navbar
+        var sticky = navbar.offsetTop;
+
+        // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+        function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+        }
+
+
+
+
 
     // Partner slider
         $('#partner-slider').owlCarousel({
@@ -87,8 +114,7 @@ $(document).ready(function( $ ){
         //Pasted content ends
 
 
-
-
+        
 
 
 
